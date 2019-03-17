35b5317c299494d4572781493930a1c4ab5d9a37
LiXiaoMing <18320429843@163.com>
2019-03-17 18:54:18 +0800

    第二次更新

--- Servlet见解/src/com/LIXIAOMING/DemoServlet.java
+++ Servlet见解/src/com/LIXIAOMING/DemoServlet.java
@@ -1,9 +1,11 @@
 package com.LIXIAOMING;
 
 import java.io.IOException;
+import java.util.Enumeration;
 
 import javax.servlet.Servlet;
 import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -31,6 +33,10 @@ public class DemoServlet implements Servlet{
 	@Override
 	public void init(ServletConfig arg0) throws ServletException {
 		// TODO Auto-generated method stub
+		arg0.getServletName();//web.xml文件配置里面servlet-name
+		arg0.getInitParameter("");//web.xml文件配置里面servlet-paramt的value值
+		arg0.getInitParameterNames();//web.xml文件配置里面servlet-name枚举（数组）
+		ServletContext servletContext = arg0.getServletContext();//servlet信息
 		
 	}
 
